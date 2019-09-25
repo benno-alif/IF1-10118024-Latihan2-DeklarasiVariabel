745174056139fd1e74ef71cf3c1bf7c8104c15d6
BennoAlif <bennoalif41@gmail.com>
2019-09-25 13:04:41 +0700

    initial commit

---
+++ src/com/benno/Main.java
@@ -0,0 +1,32 @@
+package com.benno;
+
+    /*
+    NAMA        : BENNO ALIF ANGGARA
+    KELAS       : IF1
+    NIM         : 10118024
+    Deskripsi   : Deklarasi dan Pengaksesan Variabel
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+        // Memberi nilai ke variabel
+
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+        // Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variavel karakter = " + nilaiKarakter);;
+    }
+}
